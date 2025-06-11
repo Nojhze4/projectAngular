b3f271c9ef3529a8b890562412c3b4cf12375653
Jhon Ramirez <jhramirez9716@gmail.com>
2025-06-10 21:33:18 -0500

    components

--- project-angular/src/app/app.routes.ts
+++ project-angular/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { Home } from "./components/pages/home/home";
+import { AboutUs } from "./components/about-us/about-us";
+import { Contact } from "./components/contact/contact";
+import { PageNotFound } from "./components/pages/page-not-found/page-not-found";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "about",
+        component: AboutUs,
+        title: "About | Foundations",
+    },
+    {
+        path: "contact",
+        component: Contact,
+        title: "Contact | Foundations",
+    },
+    {
+        path: "home",
+        component: Home,
+        title: "Home | Foundations",
+    },
+
+
+    {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full"
+    },
+
+    {
+        path: "**",
+        component: PageNotFound,
+        title: "Error 404 | Page Not Found",
+    },
+];
+
